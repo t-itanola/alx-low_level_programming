48b2b095e4f69514ed9f2c36a3c1694a7b9e1416
t-itanola <tayoitanola>
2023-07-17 19:55:41 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*main (void)
+ * printf function
+ * return 0
+ * end program
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

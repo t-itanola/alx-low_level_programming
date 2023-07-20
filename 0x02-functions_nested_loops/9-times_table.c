5a17ebb99f89c8002ab5c03f23db327cec2d816d
t-itanola <tayoitanola>
2023-07-19 19:12:21 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * times_table - prints the 9 times table, starting with 0
  */
@@ -21,15 +20,15 @@ _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(k + '0');
-} else if (k >= 10)
+}
+else if (k >= 10)
 {
 _putchar(',');
 _putchar(' ');
 _putchar((k / 10) + '0');
 _putchar((k % 10) + '0');
 }
-}    
+}
 _putchar('\n');
 }
-}	
-	
+}

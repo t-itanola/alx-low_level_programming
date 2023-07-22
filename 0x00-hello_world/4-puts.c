bf0972a77139afecd4103cd1fc9783d88027f341
t-itanola <tayoitanola>
2023-07-22 10:00:08 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+/*
+*int main(void)
+*is a program the uses puts as the output function of a program
+*/
 
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
